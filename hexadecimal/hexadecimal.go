ae36afc9992a08a7c2e839c7248df5e1fdde6044
Travis Clarke <travis.m.clarke@gmail.com>
2017-07-26 16:31:43 -0700

    add hexadecimal

---
+++ hexadecimal/hexadecimal.go
@@ -0,0 +1,27 @@
+/*
+
+HEXADECIMAL
+
+*/
+
+package hexadecimal
+
+// Hexadecimal2Ascii ()
+func Hexadecimal2Ascii() {
+
+}
+
+// Hexadecimal2Binary ()
+func Hexadecimal2Binary() {
+
+}
+
+// Hexadecimal2Octal ()
+func Hexadecimal2Octal() {
+
+}
+
+// Hexadecimal2Decimal ()
+func Hexadecimal2Decimal() {
+
+}
