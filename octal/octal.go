f020d27ae32baa703dff5dd7adba8da12dc4aa9b
Travis Clarke <travis.m.clarke@gmail.com>
2017-07-26 16:31:36 -0700

    add octal

---
+++ octal/octal.go
@@ -0,0 +1,27 @@
+/*
+
+OCTAL
+
+*/
+
+package octal
+
+// Octal2Ascii ()
+func Octal2Ascii() {
+
+}
+
+// Octal2Binary ()
+func Octal2Binary() {
+
+}
+
+// Octal2Decimal ()
+func Octal2Decimal() {
+
+}
+
+// Octal2Hexadecimal ()
+func Octal2Hexadecimal() {
+
+}
