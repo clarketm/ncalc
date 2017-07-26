31432799c0d69ffaf94056c1fc8677baccf3d0ad
Travis Clarke <travis.m.clarke@gmail.com>
2017-07-26 16:31:46 -0700

    add decimal

---
+++ decimal/decimal.go
@@ -0,0 +1,27 @@
+/*
+
+DECIMAL
+
+*/
+
+package decimal
+
+// Decimal2Ascii ()
+func Decimal2Ascii() {
+
+}
+
+// Decimal2Binary ()
+func Decimal2Binary() {
+
+}
+
+// Decimal2Octal ()
+func Decimal2Octal() {
+
+}
+
+// Decimal2Hexadecimal ()
+func Decimal2Hexadecimal() {
+
+}
