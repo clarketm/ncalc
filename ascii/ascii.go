873a48f25791cfbe8f20e11066af2f5825eb24a6
Travis Clarke <travis.m.clarke@gmail.com>
2017-07-26 16:31:50 -0700

    add ascii

---
+++ ascii/ascii.go
@@ -0,0 +1,27 @@
+/*
+
+ASCII
+
+*/
+
+package ascii
+
+// Ascii2Binary ()
+func Ascii2Binary() {
+
+}
+
+// Ascii2Octal ()
+func Ascii2Octal() {
+
+}
+
+// Ascii2Decimal ()
+func Ascii2Decimal() {
+
+}
+
+// Ascii2Hexadecimal ()
+func Ascii2Hexadecimal() {
+
+}
