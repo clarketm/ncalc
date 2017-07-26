8565f010c7dcd14020ff7c5eff6b2aeb508cfc3e
Travis Clarke <travis.m.clarke@gmail.com>
2017-07-26 16:31:57 -0700

    add binary

---
+++ binary/binary.go
@@ -0,0 +1,27 @@
+/*
+
+BINARY
+
+*/
+
+package binary
+
+// Binary2Ascii ()
+func Binary2Ascii() {
+
+}
+
+// Binary2Octal ()
+func Binary2Octal() {
+
+}
+
+// Binary2Decimal ()
+func Binary2Decimal() {
+
+}
+
+// Binary2Hexadecimal ()
+func Binary2Hexadecimal() {
+
+}
