c9add63c02bfa602b4c78ba5b2cd15ddac00cd5a
Travis Clarke <travis.m.clarke@gmail.com>
2017-07-28 17:57:38 -0700

    add "ascii" conversions

--- ascii/ascii.go
+++ ascii/ascii.go
@@ -6,22 +6,33 @@ ASCII
 
 package ascii
 
-// Ascii2Binary ()
-func Ascii2Binary() {
+import (
+	"fmt"
 
+	"github.com/clarketm/ncalc/decimal"
+)
+
+// Ascii2Binary (s string) int
+func Ascii2Binary(n rune) int {
+	return decimal.Decimal2Binary(int(n))
 }
 
-// Ascii2Octal ()
-func Ascii2Octal() {
-
+// Ascii2Octal (s string) int
+func Ascii2Octal(n rune) int {
+	return decimal.Decimal2Octal(int(n))
 }
 
-// Ascii2Decimal ()
-func Ascii2Decimal() {
-
+// Ascii2Decimal (c string) int
+func Ascii2Decimal(n rune) int {
+	return int(n)
 }
 
-// Ascii2Hexadecimal ()
-func Ascii2Hexadecimal() {
+// Ascii2Hexadecimal (c string) int
+func Ascii2Hexadecimal(n rune) int {
+	return decimal.Decimal2Hexadecimal(int(n))
+}
 
+// String (n string) string
+func String(n rune) string {
+	return fmt.Sprintf("%c", n)
 }
---
+++ ascii/ascii_test.go
@@ -0,0 +1,31 @@
+/*
+
+Copyright 2017 Travis Clarke. All rights reserved.
+Use of this source code is governed by a Apache-2.0
+license that can be found in the LICENSE file.
+
+*/
+
+package ascii_test
+
+import (
+	"fmt"
+
+	"github.com/clarketm/ncalc/ascii"
+)
+
+func Example() {
+	// ASCII
+	a := 'd'
+	fmt.Println(ascii.Ascii2Binary(a))
+	fmt.Println(ascii.Ascii2Octal(a))
+	fmt.Println(ascii.Ascii2Decimal(a))
+	fmt.Println(ascii.Ascii2Hexadecimal(a))
+	fmt.Printf("%v:%T", ascii.String(a), ascii.String(a))
+
+	// 1100100
+	// 144
+	// 100
+	// 64
+	// d:string
+}
