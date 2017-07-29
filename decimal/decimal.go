82c0d2c8929dcac89d7253e846bfec0426c9e09c
Travis Clarke <travis.m.clarke@gmail.com>
2017-07-28 17:57:52 -0700

    add "decimal" conversions

--- decimal/decimal.go
+++ decimal/decimal.go
@@ -6,22 +6,43 @@ DECIMAL
 
 package decimal
 
-// Decimal2Ascii ()
-func Decimal2Ascii() {
+import (
+	"fmt"
+	"strconv"
 
+	"github.com/clarketm/ncalc/utils"
+)
+
+// Decimal2Ascii (n int) string
+func Decimal2Ascii(n int) string {
+	return fmt.Sprintf("%q", n)
 }
 
-// Decimal2Binary ()
-func Decimal2Binary() {
-
+// Decimal2Binary (n int) int
+func Decimal2Binary(n int) int {
+	s := strconv.FormatInt(int64(n), utils.BINARY_BASE)
+	i, err := strconv.Atoi(s)
+	utils.CheckError(err, utils.DECIMAL, utils.BINARY)
+	return int(i)
 }
 
-// Decimal2Octal ()
-func Decimal2Octal() {
-
+// Decimal2Octal (n int) int
+func Decimal2Octal(n int) int {
+	s := strconv.FormatInt(int64(n), utils.OCTAL_BASE)
+	i, err := strconv.Atoi(s)
+	utils.CheckError(err, utils.DECIMAL, utils.OCTAL)
+	return int(i)
 }
 
-// Decimal2Hexadecimal ()
-func Decimal2Hexadecimal() {
+// Decimal2Hexadecimal (n int) int
+func Decimal2Hexadecimal(n int) int {
+	s := strconv.FormatInt(int64(n), utils.HEXADECIMAL_BASE)
+	i, err := strconv.Atoi(s)
+	utils.CheckError(err, utils.DECIMAL, utils.HEXADECIMAL)
+	return int(i)
+}
 
+// String (n int) string
+func String(n int) string {
+	return fmt.Sprintf("%d", n)
 }
---
+++ decimal/decimal_test.go
@@ -0,0 +1,33 @@
+/*
+
+Copyright 2017 Travis Clarke. All rights reserved.
+Use of this source code is governed by a Apache-2.0
+license that can be found in the LICENSE file.
+
+*/
+
+package decimal_test
+
+import (
+	"fmt"
+
+	"github.com/clarketm/ncalc/decimal"
+)
+
+func Example() {
+
+	// DECIMAL
+	d := 100
+	fmt.Println(decimal.Decimal2Ascii(d))
+	fmt.Println(decimal.Decimal2Binary(d))
+	fmt.Println(decimal.Decimal2Octal(d))
+	fmt.Println(decimal.Decimal2Hexadecimal(d))
+	fmt.Printf("%v:%T", decimal.String(d), decimal.String(d))
+
+	// Output:
+	// 'd'
+	// 1100100
+	// 144
+	// 64
+	// 100:string
+}
