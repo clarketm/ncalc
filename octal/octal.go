894f5c5be244145d685605c952276c28666c6f5c
Travis Clarke <travis.m.clarke@gmail.com>
2017-07-28 17:58:16 -0700

    add "octal" conversions

--- octal/octal.go
+++ octal/octal.go
@@ -6,22 +6,44 @@ OCTAL
 
 package octal
 
-// Octal2Ascii ()
-func Octal2Ascii() {
+import (
+	"fmt"
+	"strconv"
 
+	"github.com/clarketm/ncalc/decimal"
+	"github.com/clarketm/ncalc/utils"
+)
+
+// Octal2Ascii (n int) string
+func Octal2Ascii(n int) string {
+	return fmt.Sprintf("%q", Octal2Decimal(n))
 }
 
-// Octal2Binary ()
-func Octal2Binary() {
-
+// Octal2Binary (n int) int
+func Octal2Binary(n int) int {
+	s := strconv.Itoa(n)
+	i, err := strconv.ParseInt(s, utils.OCTAL_BASE, 0)
+	utils.CheckError(err, utils.OCTAL, utils.BINARY)
+	return decimal.Decimal2Binary(int(i))
 }
 
-// Octal2Decimal ()
-func Octal2Decimal() {
-
+// Octal2Decimal (n int) int
+func Octal2Decimal(n int) int {
+	s := strconv.Itoa(n)
+	i, err := strconv.ParseInt(s, utils.OCTAL_BASE, 0)
+	utils.CheckError(err, utils.OCTAL, utils.DECIMAL)
+	return int(i)
 }
 
-// Octal2Hexadecimal ()
-func Octal2Hexadecimal() {
+// Octal2Hexadecimal (n int) int
+func Octal2Hexadecimal(n int) int {
+	s := strconv.Itoa(n)
+	i, err := strconv.ParseInt(s, utils.OCTAL_BASE, 0)
+	utils.CheckError(err, utils.OCTAL, utils.HEXADECIMAL)
+	return decimal.Decimal2Hexadecimal(int(i))
+}
 
+// String (n int) string
+func String(n int) string {
+	return fmt.Sprintf("%o", Octal2Decimal(n))
 }
---
+++ octal/octal_test.go
@@ -0,0 +1,33 @@
+/*
+
+Copyright 2017 Travis Clarke. All rights reserved.
+Use of this source code is governed by a Apache-2.0
+license that can be found in the LICENSE file.
+
+*/
+
+package octal_test
+
+import (
+	"fmt"
+
+	"github.com/clarketm/ncalc/octal"
+)
+
+func Example() {
+
+	// OCTAL
+	o := 144
+	fmt.Println(octal.Octal2Ascii(o))
+	fmt.Println(octal.Octal2Binary(o))
+	fmt.Println(octal.Octal2Decimal(o))
+	fmt.Println(octal.Octal2Hexadecimal(o))
+	fmt.Printf("%v:%T", octal.String(o), octal.String(o))
+
+	// Output:
+	// 'd'
+	// 1100100
+	// 100
+	// 64
+	// 144:string
+}
