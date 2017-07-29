db0d138aa4605aa78bfb8831f68991dd7bc87393
Travis Clarke <travis.m.clarke@gmail.com>
2017-07-28 17:57:46 -0700

    add "binary" conversions

--- binary/binary.go
+++ binary/binary.go
@@ -6,22 +6,44 @@ BINARY
 
 package binary
 
-// Binary2Ascii ()
-func Binary2Ascii() {
+import (
+	"fmt"
+	"strconv"
 
+	"github.com/clarketm/ncalc/decimal"
+	"github.com/clarketm/ncalc/utils"
+)
+
+// Binary2Ascii (n int) string
+func Binary2Ascii(n int) string {
+	return fmt.Sprintf("%q", Binary2Decimal(n))
 }
 
-// Binary2Octal ()
-func Binary2Octal() {
-
+// Binary2Octal (n int) int
+func Binary2Octal(n int) int {
+	s := strconv.Itoa(n)
+	i, err := strconv.ParseInt(s, utils.BINARY_BASE, 0)
+	utils.CheckError(err, utils.BINARY, utils.OCTAL)
+	return decimal.Decimal2Octal(int(i))
 }
 
-// Binary2Decimal ()
-func Binary2Decimal() {
-
+// Binary2Decimal (n int) int
+func Binary2Decimal(n int) int {
+	s := strconv.Itoa(n)
+	i, err := strconv.ParseInt(s, utils.BINARY_BASE, 0)
+	utils.CheckError(err, utils.BINARY, utils.DECIMAL)
+	return int(i)
 }
 
-// Binary2Hexadecimal ()
-func Binary2Hexadecimal() {
+// Binary2Hexadecimal (n int) int
+func Binary2Hexadecimal(n int) int {
+	s := strconv.Itoa(n)
+	i, err := strconv.ParseInt(s, utils.BINARY_BASE, 0)
+	utils.CheckError(err, utils.BINARY, utils.HEXADECIMAL)
+	return decimal.Decimal2Hexadecimal(int(i))
+}
 
+// String (n int) string
+func String(n int) string {
+	return fmt.Sprintf("%b", Binary2Decimal(n))
 }
---
+++ binary/binary_test.go
@@ -0,0 +1,33 @@
+/*
+
+Copyright 2017 Travis Clarke. All rights reserved.
+Use of this source code is governed by a Apache-2.0
+license that can be found in the LICENSE file.
+
+*/
+
+package binary_test
+
+import (
+	"fmt"
+
+	"github.com/clarketm/ncalc/binary"
+)
+
+func Example() {
+
+	// BINARY
+	b := 1100100
+	fmt.Println(binary.Binary2Ascii(b))
+	fmt.Println(binary.Binary2Octal(b))
+	fmt.Println(binary.Binary2Decimal(b))
+	fmt.Println(binary.Binary2Hexadecimal(b))
+	fmt.Printf("%v:%T", binary.String(b), binary.String(b))
+
+	// Output:
+	// 'd'
+	// 144
+	// 100
+	// 64
+	// 1100100:string
+}
