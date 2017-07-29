7a87f47d1e243d48a58adfee9ad502d7ee99909e
Travis Clarke <travis.m.clarke@gmail.com>
2017-07-28 21:24:44 -0700

    add Invoke function call reflection

--- utils/functions.go
+++ utils/functions.go
@@ -3,7 +3,19 @@ package utils
 import (
 	"fmt"
 	"os"
+	"reflect"
 )
+
+// Invoke (fn interface{}, args ...interface{}) interface{}
+func Invoke(fn interface{}, args ...interface{}) interface{} {
+	v := reflect.ValueOf(fn)
+	rargs := make([]reflect.Value, len(args))
+	for i, a := range args {
+		rargs[i] = reflect.ValueOf(a)
+	}
+	defer recover()
+	return v.Call(rargs)[0]
+}
 
 // CheckError (err error, formatSrc, formatDst string)
 func CheckError(err error, formatSrc, formatDst string) {
