7ff7a5143789a2e5d52ff7c4cb8b11bb7cc99533
Travis Clarke <travis.m.clarke@gmail.com>
2017-07-29 00:34:00 -0700

    add manpage documentation

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
 ```shell
 NAME:
     ncalc – number base converter.
+
+SYNOPSIS:
+    ncalc [ opts... ] [ number|character ]
+
+OPTIONS:
+    -h, --help                  print usage.
+    -i, --input format          input format. see FORMATS. (default: decimal|ascii)
+    -o, --output format         output format. see FORMATS. (default: all)
+    -v, --version               print version number.
+
+FORMATS:
+    (a)scii                     character
+    (b)inary                    base 2
+    (o)ctal                     base 8
+    (d)ecimal                   base 10
+    (h)exadecimal               base 16
+
+EXAMPLES:
+    ncalc "6"                               # output `decimal` number `6` in `all` formats
+    ncalc "G"                               # output `ascii` character `G` in `all` formats
+    ncalc -i a "f"                          # output `ascii` character `f` in `all` formats
+    ncalc -i decimal -o ascii "15"          # output `decimal` number `15` as `ascii`
+    ncalc --input h --output o "ff"         # output `hexadecimal` number `ff` as `octal`
 ```
--- main.go
+++ main.go
@@ -4,7 +4,31 @@ Copyright 2017 Travis Clarke. All rights reserved.
 Use of this source code is governed by a Apache-2.0
 license that can be found in the LICENSE file.
 
-TODO: Manpage
+NAME:
+    ncalc – number base converter.
+
+SYNOPSIS:
+    ncalc [ opts... ] [ number|character ]
+
+OPTIONS:
+    -h, --help                  print usage.
+    -i, --input format          input format. see FORMATS. (default: decimal|ascii)
+    -o, --output format         output format. see FORMATS. (default: all)
+    -v, --version               print version number.
+
+FORMATS:
+    (a)scii                     character
+    (b)inary                    base 2
+    (o)ctal                     base 8
+    (d)ecimal                   base 10
+    (h)exadecimal               base 16
+
+EXAMPLES:
+    ncalc "6"                               # output `decimal` number `6` in `all` formats
+    ncalc "G"                               # output `ascii` character `G` in `all` formats
+    ncalc -i a "f"                          # output `ascii` character `f` in `all` formats
+    ncalc -i decimal -o ascii "15"          # output `decimal` number `15` as `ascii`
+    ncalc --input h --output o "ff"         # output `hexadecimal` number `ff` as `octal`
 
 */
 
