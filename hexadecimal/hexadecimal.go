4884a2f1dddc3baa4b56778c44f86dc6c16ca28d
Travis Clarke <travis.m.clarke@gmail.com>
2017-07-28 17:58:05 -0700

    add "hexadecimal" conversions

--- hexadecimal/hexadecimal.go
+++ hexadecimal/hexadecimal.go
@@ -6,22 +6,44 @@ HEXADECIMAL
 
 package hexadecimal
 
-// Hexadecimal2Ascii ()
-func Hexadecimal2Ascii() {
+import (
+	"fmt"
+	"strconv"
 
+	"github.com/clarketm/ncalc/decimal"
+	"github.com/clarketm/ncalc/utils"
+)
+
+// Hexadecimal2Ascii (n int) string
+func Hexadecimal2Ascii(n int) string {
+	return fmt.Sprintf("%q", Hexadecimal2Decimal(n))
 }
 
-// Hexadecimal2Binary ()
-func Hexadecimal2Binary() {
-
+// Hexadecimal2Binary (n int) int
+func Hexadecimal2Binary(n int) int {
+	s := strconv.Itoa(n)
+	i, err := strconv.ParseInt(s, utils.HEXADECIMAL_BASE, 0)
+	utils.CheckError(err, utils.HEXADECIMAL, utils.BINARY)
+	return decimal.Decimal2Binary(int(i))
 }
 
-// Hexadecimal2Octal ()
-func Hexadecimal2Octal() {
-
+// Hexadecimal2Octal (n int) int
+func Hexadecimal2Octal(n int) int {
+	s := strconv.Itoa(n)
+	i, err := strconv.ParseInt(s, utils.HEXADECIMAL_BASE, 0)
+	utils.CheckError(err, utils.HEXADECIMAL, utils.OCTAL)
+	return decimal.Decimal2Octal(int(i))
 }
 
-// Hexadecimal2Decimal ()
-func Hexadecimal2Decimal() {
+// Hexadecimal2Decimal (n int) int
+func Hexadecimal2Decimal(n int) int {
+	s := strconv.Itoa(n)
+	i, err := strconv.ParseInt(s, utils.HEXADECIMAL_BASE, 0)
+	utils.CheckError(err, utils.HEXADECIMAL, utils.DECIMAL)
+	return int(i)
+}
 
+// String (n int) string
+func String(n int) string {
+	return fmt.Sprintf("%x", Hexadecimal2Decimal(n))
 }
---
+++ hexadecimal/hexadecimal_test.go
@@ -0,0 +1,33 @@
+/*
+
+Copyright 2017 Travis Clarke. All rights reserved.
+Use of this source code is governed by a Apache-2.0
+license that can be found in the LICENSE file.
+
+*/
+
+package hexadecimal_test
+
+import (
+	"fmt"
+
+	"github.com/clarketm/ncalc/hexadecimal"
+)
+
+func Example() {
+
+	// HEXADECIMAL
+	h := 64
+	fmt.Println(hexadecimal.Hexadecimal2Ascii(h))
+	fmt.Println(hexadecimal.Hexadecimal2Binary(h))
+	fmt.Println(hexadecimal.Hexadecimal2Octal(h))
+	fmt.Println(hexadecimal.Hexadecimal2Decimal(h))
+	fmt.Printf("%v:%T", hexadecimal.String(h), hexadecimal.String(h))
+
+	// Output:
+	// 'd'
+	// 1100100
+	// 144
+	// 100
+	// 64:string
+}
