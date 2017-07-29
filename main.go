ee036e8c17eea6f4aa4fc629f0a1459b69bf5a3a
Travis Clarke <travis.m.clarke@gmail.com>
2017-07-28 17:58:28 -0700

    rename ncalc to main

---
+++ main.go
@@ -0,0 +1,16 @@
+/*
+
+Copyright 2017 Travis Clarke. All rights reserved.
+Use of this source code is governed by a Apache-2.0
+license that can be found in the LICENSE file.
+
+TODO: Manpage
+
+*/
+
+package main
+
+// main ()
+func main() {
+	println("ran")
+}
