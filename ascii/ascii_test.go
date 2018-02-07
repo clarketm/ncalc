382535112360897c615fcf08546c53a589b5d7e3
Travis Clarke <travis.m.clarke@gmail.com>
2018-02-07 15:12:46 -0800

    update copyright year

--- ascii/ascii_test.go
+++ ascii/ascii_test.go
@@ -1,6 +1,6 @@
 /*
 
-Copyright 2017 Travis Clarke. All rights reserved.
+Copyright 2018 Travis Clarke. All rights reserved.
 Use of this source code is governed by a Apache-2.0
 license that can be found in the LICENSE file.
 
